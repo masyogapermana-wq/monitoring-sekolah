731bb167565d1b01804e33f6443dc9360359d9d3
Yoga Permana <masyogapermana@gmail.com>
2026-06-15 16:55:07 +0700

    fix-logo2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,8 +21,6 @@
             box-shadow: 0 4px 6px rgba(0,0,0,0.1);
         }
         .icon-logo {
-            font-size: 4rem; /* Ukuran icon besar */
-            color: #0d6efd; /* Warna biru primary */
             margin-bottom: 10px;
         }
     </style>
